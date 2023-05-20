98f7f9b28f64048bad226b6dc0220c7d1d7767e9
mvoipio <74784051+mvoipio@users.noreply.github.com>
2023-05-20 22:32:15 +0300

    Almost there!
    Sovellus vaikuttaa valmiilta, readme on vielä kesken.

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
-# lautanauhalaskuri
-JavaScript/jQuery thingy that counts materials needed for a given tablet woven band
+# Lautanauhalaskuri
+
+Lautanauhalaskuri on JavaScriptillä ja jQueryllä toteteutettu pikkuohjelma, jolla voi laskea erilaisia asioita
+lautanauhaloimea valmistellessaan: kuinka pitkä loimen pitää olla, että valmis nauha on x senttiä pitkä, kuinka monta
+metriä nauhan eri värejä tarvitaan yhteensä nauhaloimeen, paljonko kudelankaa pitää olla, ja lopuksi sen, montako
+grammaa lankaa tuo x metriä on.
+
+
+## Loimen pituuden laskuri
+
+Laskurille annetaan toivottu valmiin nauhan pituus ja laskuri lisää siihen kutistumisvaran sekä loimen
+alkuun ja loppuun tarvittavat varat ("tutkaimet"). Pituuden oletetaan asettuvan välille 10-1500 cm,
+hyvin harva haluaa ikinä tehdä yli 15-metristä lointaa, yli viisimetrinenkin on harvinaisuus. Jos
+haluaa tehdä samasta loimesta useita nauhoja tupsuilla tai muuten haluaa vähän ylimääräistä (kuten minä,
+joka sössin joka nauhan alun jotenkin), voi loimelle asettaa 1-150 cm lisäpituutta.
+
+Laskurin käytön jälkeen voi loimen mitan kopioida leikepöydälle. Laskurin voi myös tyhjentää ja aloittaa alusta.
+Laskurin antamaa loimen pituutta käytetään seuraavassa vaiheessa, ellei käyttäjä itse muuta sitä.
+
+
+## Eriväristen lankojen yhteispituuden laskuri
+
+Laskurin tätä osuutta voi käyttää ykkösosaan koskematta, jos asettaa toivotun loimen pituuden käsin.
+
+Riippumatta siitä, miten loimen pituus määritellään, seuraava vaihe toimii aina samoin. Taulukkoon kirjoitettavat
+tiedot tulevat valitusta nauhamallista. Ensimmäiseen sarakkeeseen kirjoitetaan värin nimi juuri sillä tavalla kuin
+käyttäjä haluaa. Toiseen sarakkeeseen kirjoitetaan sen värin lankojen määrä. Rivejä saa lisää Lisää rivi-painikkeella
+ja lisätyn rivin saa poistettua rivin päässä olevalla poisto-painikkeella.
+
+Kun värit ja lankalukumäärät on syötetty, lasketaan pituudet ja tehdään niistä taulukko. Taulukon alle tulostuu myös
+muutamia yhteenvetotietoja, kuten laskelmiin käytetty loimen pituus ja lankojen kokonaismäärä sekä arvioitu kuteen määrä.
+Taulukon ja sen alla olevat yhteenvetotiedot voi kätevästi tulostaa tulostuspainikkeella. Taulukkojen täytön
+voi myös aloittaa alusta tyhjentämällä tiedot.
+
+
+## Langan painon laskuri
+
+Tyypillisesti neulontalankakerässä olevan langan määrä ilmoitetaan grammojen metrimäärinä, esim. 100 g / 400 m.
+(Kankaan)kudontaan tarkoitettujen lankojen paksuus ja säikeiden määrä taas ilmoitetaan tex-luvulla, 1 TEX = 1 g/1000 m.
+Jälkimmäinen numero taas kertoo säikeiden määrän, eli 125 tex x 2 tarkoittaa 125 g/1000 m * 2.
+
+Koska meidän tapauksessamme käyttäjä tietää, että hän tarvitsee esimerkiksi 80 metriä sinistä lankaa, hän voi ottaa
+juoksevuustiedot lankakerän kyljestä, syöttää ne loimen pituuden kanssa laskuriin ja vastauksena tulee suuntaa-antava
+arvio siitä, kuinka monta grammaa lankaa loimeen tarvitaan. Koska langan paino voi vaihdella esimerkiksi ilmankosteuden
+mukaan, kyse ei ole eksaktista tieteestä ja tulokset pyöristetäänkin ylöspäin lähimpään grammaan.
+
+
+## Toteutustapa
+
+Aloitin puhtaasta html-sivusta, jolla oli vain css-tyylitiedosto. Aloitin JavaScriptin kirjoittamisen sivun alusta ja
+pyrin käyttämään jQueryä aina kuin mahdollista. Leikepöydälle kopioinnin ja tulostusmuotoilujen suhteen oli helpompaa
+käyttää puhdasta JavaScriptiä, muu on tehty jQueryn avulla.
+
+### Lähteet
+
+En ole lahjakas ohjelmoija, joten Google on paras ystäväni. Tässä apuna käyttämäni verkkosivut siinä järjestyksessä,
+kun niitä tarvitsin.
+
+
+* Adding jQuery to Your Web Pages: https://www.w3schools.com/jquery/jquery_get_started.asp
+
+* Google Hosted Libraries: https://developers.google.com/speed/libraries
+
+* Calculation with Jquery: https://codereview.stackexchange.com/questions/192317/calculation-with-jquery
+
+* How to force addition instead of concatenation in javascript: https://stackoverflow.com/questions/13953939/how-to-force-addition-instead-of-concatenation-in-javascript
+
+* jQuery, .val(): http://api.jquery.com/val/
+
+* How to Check If an Input Field is Empty Using jQuery: https://www.tutorialrepublic.com/faq/how-to-check-if-an-input-field-is-empty-using-jquery.php
+
+* How to set value of variable in Html element with jquery?: https://stackoverflow.com/questions/51041561/how-to-set-value-of-variable-in-html-element-with-jquery
+
+* How to Copy to Clipboard in JavaScript with the Clipboard API: https://stackabuse.com/how-to-copy-to-clipboard-in-javascript-with-the-clipboard-api/
+
+* How to empty input field with jQuery: https://stackoverflow.com/questions/9236332/how-to-empty-input-field-with-jquery
+
+* jQuery empty() Method: https://www.w3schools.com/jquery/html_empty.asp
+
+* How to Dynamically Add/Remove Table Rows using jQuery?: https://www.geeksforgeeks.org/how-to-dynamically-add-remove-table-rows-using-jquery/
+
+* How do i declare a global variable in jquery?: https://forum.jquery.com/topic/how-do-i-declare-a-global-variable-in-jquery
+
+* How to Check Empty, Null, and Undefined Variables in Javascript / jQuery?: https://dev.to/codeanddeploy/how-to-check-empty-null-and-undefined-variables-in-javascript-jquery-5agl
+
+* Chrome violation: [Violation] Handler took 83ms of runtime: https://stackoverflow.com/questions/42218699/chrome-violation-violation-handler-took-83ms-of-runtime
+(It only shows when console is set to "Verbose", can be hidden.)
+
+* How to get data from a table and move to another table in jquery: https://stackoverflow.com/questions/54421758/how-to-get-data-from-a-table-and-move-to-another-table-in-jquery
+
+* Select inputs with number type through jQuery: https://stackoverflow.com/questions/4887085/select-inputs-with-number-type-through-jquery
+
+* What is the best way to remove a table row with jQuery?: https://stackoverflow.com/questions/170997/what-is-the-best-way-to-remove-a-table-row-with-jquery
+
+* How to scroll up or down the page to an anchor using jQuery?: https://stackoverflow.com/questions/8579643/how-to-scroll-up-or-down-the-page-to-an-anchor-using-jquery
+
+* Round up final output in jquery: https://stackoverflow.com/questions/13644341/round-up-final-output-in-jquery
+
+
+
+// 18 pomodoroa -> minimum viable product
+// 24 pomodoroa -> melkein valmis, readme vielä vaiheessa
+
+
+Näitäkin katsoin, mutten sitten kuitenkaan käyttänyt:
+
+Color picker is by Victor Maestri, I found it at https://www.jqueryscript.net/other/pick-color-predefined-palette.html.
+(Tämän saatan vielä ottaa käyttöön, pohdin sitä myöhemmin.)
+
+Tooltip message on hover: https://stackoverflow.com/questions/1333546/how-can-i-display-a-tooltip-message-on-hover-using-jquery 
+
+jQuery Validation plugin: https://jqueryvalidation.org/documentation/
+https://webdesign.tutsplus.com/tutorials/easy-form-validation-with-jquery--cms-33096
+https://www.sitepoint.com/basic-jquery-form-validation-tutorial/
+
---
+++ js/jscript.js
@@ -0,0 +1,354 @@
+// Skriptissä on käytetty jQueryä aina kun mahdollista
+
+var lpituus; // loimen pituus (joka lasketaan/asetetaan myöhemmin) on globaali muuttuja
+
+// Pituuslaskuri (sivun ensimmäinen osa)
+
+
+$(document).ready(function(){
+$('#laskepituus').click(function () {
+    var alkuPituus = parseInt($('#tpituus').val());
+
+        // Tarkistetaan, että perusmitta on annettu ja se on välissä 10-1500
+        if(alkuPituus==null || alkuPituus==""){
+            alert("Valmiin nauhan pituus tarvitaan");
+            return false;
+        } else if (alkuPituus < 10 || alkuPituus > 1500){
+            alert("Syötäthän nauhan toivotun pituuden välillä 10-1500");
+            return false;  
+        }
+
+
+    if(!$('#lpituus').val()){ // if input is empty, .val returns "undefined", so empty must be given value 0
+        var lisaPituus = 0;
+    } else {
+        var lisaPituus = parseInt($('#lpituus').val());
+    }
+
+
+    if(lisaPituus < 0 || lisaPituus > 150){
+        alert("Syötäthän lisäpituuden väliltä 1-150");
+        return false;
+    } 
+
+    lpituus = parseInt(alkuPituus * 1.3 + 65 + lisaPituus) ;
+
+
+    $('#vpituus').text(lpituus);
+
+    $('#laskutulos').show();
+
+    $('#apituus').text(lpituus); // toistaa pituuden seuraavassa osassa - tämän voisi ehkä hoitaa fiksumminkin
+
+    $('#toistapituus').show(); // näyttää seuraavan osan alussa tekstin, jossa kerrotaan, mikä laskelmiin käytetty pituus on
+
+    $('#kopioipituus').show(); // näyttää kopiointinapin
+    $('#tyhjennaloimi').show(); // näyttää tyhjennysnapin
+});
+});
+
+
+// Kopioi loimen pituuden leikepöydälle
+// document.execCommand() on vanhentunut, täytyy käyttää Clipboard APIa
+
+function kopioiPituus() {
+    var copyText = $('#vpituus').text();
+    navigator.clipboard.writeText(copyText).then(() => {
+        // Alert the user that the action took place.
+        // Nobody likes hidden stuff being done under the hood!
+        alert('Kopioitu: ' + copyText);
+    });
+  }
+
+  $("div").empty(); // ei enää muista, mitä tämä tekee - onko tarpeellinen?
+
+  // resetoi laskurin ja tulokset
+
+  $(document).ready(function(){
+  $('#tyhjennaloimi').click(function () {
+
+    $('#pituuslaskuri')[0].reset(); // tyhjentää laskurin
+    $('#vpituus').empty(); // ylempi laskutulos pois
+    $('#apituus').empty(); // alempi laskutulos pois
+    $('#laskutulos').hide(); // piilottaa tulosrivin 
+    $('#toistapituus').hide(); // piilottaa myös kakkososan alussa olevan tiedon loimen pituudesta
+    $('#kopioipituus').hide(); // piilottaa kopiointinapin
+    $('#tyhjennaloimi').hide(); // piilottaa tyhjennysnapin
+  
+  });
+});
+
+// vaihtaa loimen pituuden ja nollaa vanhan
+
+$(document).ready(function(){
+    $('#vaihdapituus').click(function () {
+        lpituus = parseFloat($('#upituus').val());
+        
+        $('#fpituus').text(lpituus);
+    
+        $('#naytauusipituus').show();
+
+        $('#toistapituus').hide(); // piilottaa edellisestä pituudesta kertovan rivin/kappaleen
+    
+    });
+    });
+
+
+// TODO: Tyhjennysnappi tehty, muttei ei toiminnallisuutta!
+
+
+// Dynaaminen taulukko
+// Suurin osa koodista täältä: https://www.geeksforgeeks.org/how-to-dynamically-add-remove-table-rows-using-jquery/    
+
+// Rivien kokonaismäärä
+var rowIdx = 0;
+  
+// Rivin lisääminen
+$(document).ready(function(){
+$('#lisaarivi').on('click', function () {
+  
+    // Uusi rivi tbodyn sisään
+    $('#varitaulukko').append(`<tr id="R${++rowIdx}">
+          <td><input type="text"></td>
+           <td><input class="number" type="number"></td>
+           <td class="trashcan"><button class="trash"><i class="fa-solid fa-trash-can"></i></button></td>
+           </tr>`);
+});
+});
+
+
+// Rivin poistaminen
+$(document).ready(function(){
+$('#varitaulukko').on('click', '.trash', function () {
+  
+    // Getting all the rows next to the 
+    // row containing the clicked button
+    var child = $(this).closest('tr').nextAll();
+  
+    // Iterating across all the rows 
+    // obtained to change the index
+    child.each(function () {
+          
+        // Getting <tr> id.
+        var id = $(this).attr('id');
+  
+        // Getting the <p> inside the .row-index class.
+        var idx = $(this).children('.row-index').children('p');
+  
+        // Gets the row number from <tr> id.
+        var dig = parseInt(id.substring(1));
+  
+        // Modifying row index.
+        idx.html(`Row ${dig - 1}`);
+  
+        // Modifying row id.
+        $(this).attr('id', `R${dig - 1}`);
+    });
+  
+    // Removing the current row.
+    $(this).closest('tr').remove();
+  
+    // Decreasing the total number of rows by 1.
+    rowIdx--;
+});
+
+});
+
+
+// TODO: Lankojen lukumäärän validaatio jo syöttövaiheessa?
+// Validaatio tarvitaan joka tapauksessa!
+
+$(document).ready(function(){
+    $("#laskelangat").click(function(){  
+
+    $("#loputtulokset").empty(); // Poistaa tulostaulukosta dynaamiset rivit, muuten append aiheuttaa ongelmia
+
+    // tarkistetaan, että loimen pituudelle on annettu joku arvo joko laskurilla tai käsin
+    if (typeof lpituus == "undefined"){
+        alert('Loimen pituutta ei ole määritetty, aseta tai laske loimen pituus'); 
+        return false;
+    }
+
+
+    //TODO: Kaikki mitat muutettava metreiksi!
+
+    var ekamaara = parseInt($('#lmaara1').val());
+    if (ekamaara > 0 && ekamaara < 81){ //tarkistaa, että lankojen lukumäärä on annettu
+        ekamaara = ekamaara;   
+    } else {
+        alert('Anna ensimmäisen rivin lankojen määrä väliltä 1-80');
+        return false;
+    }
+
+    var ekapituus = parseInt(ekamaara * lpituus) ;
+    ekapituus = Math.ceil(ekapituus / 100) ; // muutetaan pituus metreiksi ja pyöristetään ylös lähimpään metriin
+    
+
+    $('#vari1tulos').text($('#vari1').val());  //kopioi kirjoitetun tekstin suoraan uuteen soluun
+    $('#lanka1tulos').text(ekamaara);
+    $('#lanka1pituus').text(ekapituus + " m");
+
+    var kmaara = 0;
+    var kpituus = 0;
+
+
+
+    $("table #varitaulukko tr").each(function(){
+            
+          var lvari=$(this).find('input[type=text]').val(); //saattaa purra, jos ykkösruutuun tuleekin väripalkki
+          var lmaara=parseInt($(this).find('input[type=number]').val()); //täytyy lukea numeroksi
+          if (lmaara > 0 && lmaara < 81){ //tarkistaa, että lankojen lukumäärä on annettu
+            lmaara = lmaara;   
+        } else {
+            alert('Joltakin lisätyltä riviltä puuttuu lankojen määrä tai se ei ole väliltä 1-80');           
+            return false;
+        }
+
+        var loppituus = Math.ceil(lmaara * lpituus / 100); // muutetaan metreiksi ja pyöristetään ylöspäin
+           $("#loputtulokset").append("<tr><td>"+ lvari +"</td><td>" + lmaara + "</td><td>" + loppituus + " m</td></tr>")
+
+           kmaara += lmaara;
+           kpituus += loppituus;
+         
+        });
+
+        kmaara = kmaara + ekamaara; // täytyy muistaa lisätä ensimmäisen solun lankojen määrä
+        kpituus = kpituus + ekapituus; // täytyy muistaa lisätä ensimmäisen solun lankojen pituus
+        kude = kpituus / 10; // kuteen määrä on noin 10 % loimen lankojen yhteispituudesta, muutetaan metreiksi
+
+        // Näytetään edellä koottu taulukko
+
+        $('#pituustaulukko').show();
+
+ 
+        // Lisätään kokonaismäärät ja näytetään ne
+        $('#pituusf').text(lpituus); // käytetty loimen (ei loimilankojen) kokonaispituus, sama kuin aiemmin
+        $('#lankamaara').text(kmaara); // lankojen yhteismäärä (hyvä tarkistusluku)
+        $('#langatyht').show(); // näytetään em. tiedot
+
+        $('#kude').text(kude); // kuteen pituus lisätään sivulle
+        $('#kudelaskelma').show(); // näytetään kudelaskelma
+
+        $('#tulostataulukko').show(); // näyttää tulostusnapin
+        $('#tyhjennaloimet').show(); // näyttää tyhjennysnapin
+
+    });
+
+});
+
+
+
+// Tulostus olikin sitten helpompi hoitaa vanhaan tapaan pelkällä JavaScriptillä
+// PrintThis-plugin olisi kyllä tarjolla: https://github.com/jasonday/printThis
+
+// Tulostaa annetun (yhden) elementin sisällön CSS-tyylitiedostoa käyttäen
+// Koodi lähes suoraan StackExchangestä: https://stackoverflow.com/questions/21379605/printing-div-content-with-css-applied
+function tulosta(){
+
+    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+    mywindow.document.write('<html><head>');
+    mywindow.document.write("<link rel=\"stylesheet\" link href=\"../css/print.css\"> <link rel=\"stylesheet\" link href=\".css/layout.css\">");
+    mywindow.document.write('</head><body >');
+    mywindow.document.write('<h1>Lautanauhan loimilaskuri</h1>')
+    mywindow.document.write(document.getElementById('tulostettava').innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+
+    setTimeout(function () {
+    mywindow.print();
+    mywindow.close();
+    }, 1000)
+    return true;
+}
+
+
+// Laskurin tyhjentäminen (ei tyhjennä loimen pituuden arvoa lpituus)
+$(document).ready(function(){
+    $('#tyhjennaloimet').click(function () {
+
+      $('#lankalaskuri')[0].reset(); // Tyhjentää laskuritaulukon näkyviin jäävän osan
+      $("#varitaulukko").empty(); // Poistaa laskuritaulukkoon myöhemmin lisätyt rivit
+      $('#pituustaulukko').hide(); //piilotetaan pituuslaskelmataulukko kokonaan
+      $('#langatyht').hide(); // piilotetaan lankamäärä ja käytetty loimen mitta
+      $('#kudelaskelma').hide(); // piilotetaan kudelaskelma
+      $('#tulostataulukko').hide(); // piilottaa tulostusnapin
+      $('#tyhjennaloimet').hide(); // piilottaa tyhjennysnapin
+
+      // Tyhjentäminen tekee pahoja sivun pituudelle, joten lopuksi mennään laskurin alkuun
+        var tag = $("#lankalaskurialue");
+        $('html,body').animate({scrollTop: tag.offset().top},'slow');
+    
+    });
+  });
+  
+
+$(document).ready(function(){
+    $('#laskegrammatmetreista').click(function () {
+    var tmetrit = parseInt($('#langanpituus').val());  // tarvittava metrimäärä
+    var lmetrit = parseInt($('#metrit').val()); // kerän metrimäärä langan tiedoista
+    var grammat = parseInt($('#grammat').val()); // kerän grammakoko langan tiedoista
+
+    grammamaara = Math.ceil(tmetrit * grammat / lmetrit) ; // lasketaan grammat ja pyöristetään ne ylös lähimpään kokonaislukuun
+
+    $('#painometreista').text(grammamaara); // lisätään numero tekstiin
+    $('#ltarve').text(tmetrit);
+    $('#lankametrit').text(lmetrit);
+    $('#lankagrammat').text(grammat);
+    $('#laskutulosmetreista').show(); // näytetään teksti
+
+    $('#tyhjennamgrammat').show(); // Näytetään laskurin tyhjennysnappi
+
+    });
+});
+
+// TODO: Laskutuloksen pyöristys täysiin grammoihin!
+
+
+$(document).ready(function(){
+    $('#tyhjennamgrammat').click(function () {
+  
+      $('#grammalaskuri')[0].reset(); // tyhjentää laskurin
+      $('#laskutulosmetreista').hide(); // piilottaa tulosrivin 
+      $('#tyhjennamgrammat').hide(); // piilottaa tyhjennysnapin
+    
+    });
+  });
+
+
+  $(document).ready(function(){
+    $('#lasketulostexista').click(function () {
+    var texmetrit = parseInt($('#ltlanganpituus').val());  // tarvittava metrimäärä
+    var tex1 = parseInt($('#tex1').val()); // tex-luku
+    var tex2 = parseInt($('#tex2').val()); // säiemäärä
+
+    texgrammat = Math.ceil(texmetrit * tex1 * tex2 / 1000) ; // lasketaan grammat ja pyöristetään tulos kokonaisluvuksi ylöspäin
+
+    $('#painotexista').text(texgrammat); // lisätään numero tekstiin
+    $('#lttarve').text(texmetrit);
+    $('#ltex1').text(tex1);
+    $('#ltex2').text(tex2);
+    $('#laskutulostexista').show(); // näytetään teksti
+
+    $('#tyhjennatexit').show(); // Näytetään laskurin tyhjennysnappi
+
+ //   console.log(tmetrit, grammat, lmetrit);
+ //   console.log(grammamaara);
+    });
+});
+
+// TODO: Laskutuloksen pyöristys täysiin grammoihin!
+
+
+$(document).ready(function(){
+    $('#tyhjennatexit').click(function () {
+  
+      $('#texlaskuri')[0].reset(); // tyhjentää laskurin
+      $('#laskutulostexista').hide(); // piilottaa tulosrivin 
+      $('#tyhjennatexit').hide(); // piilottaa tyhjennysnapin
+    
+    });
+  }); 
